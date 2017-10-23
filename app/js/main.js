708e4ba2d209137d043613944edca1d33484510a
YuryKoropenko <yurykoropenko@gmail.com>
2017-10-23 17:06:58 +0300

    23.10.2017

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	/*header-nav*/
+	$('.h-nav__mob').on('click', function() {
+		$(this).parent().children('.h-nav').slideToggle();
+		return false;
+	});
+});
