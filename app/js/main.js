804e6198f07b450595840925efe1d4dce683f86f
YuryKoropenko <yurykoropenko@gmail.com>
2017-10-24 17:12:30 +0300

    все страницы

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-	/*header-nav*/
-	$('.h-nav__mob').on('click', function() {
-		$(this).parent().children('.h-nav').slideToggle();
-		return false;
-	});
+
+	if ($(window).width() > 991) {
+		/*header-subnav*/
+		$('.h-nav__item').hover(function() {
+			$(this).children('.h-subnav').stop(false, true).fadeIn();
+			}, function() {
+				$(this).children('.h-subnav').stop(false, true).fadeOut();
+		});
+	}
+	if ($(window).width() < 991) {
+		/*header-nav*/
+		$('.h-nav__mob').on('click', function() {
+			$(this).parent().children('.h-nav').slideToggle();
+			return false;
+		});
+
+		/*header-subnav*/
+		$('.h-nav__item').on('click', function() {
+			$(this).children('.h-subnav').stop(false, true).slideToggle();
+			return false;
+		});
+	}
+
+	$('.p-photo__link').fancybox();
 });
