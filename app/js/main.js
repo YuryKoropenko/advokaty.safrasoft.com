163ae97388d7a1802325b6e6a6e98556a50f1de1
YuryKoropenko <yurykoropenko@gmail.com>
2017-10-26 10:28:59 +0300

    новое меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
-	if ($(window).width() > 991) {
+	if ($(window).width() > 768) {
 		/*header-subnav*/
 		$('.h-nav__item').hover(function() {
+			$(this).addClass('h-nav__item-active');
 			$(this).children('.h-subnav').stop(false, true).fadeIn();
 			}, function() {
+				$(this).removeClass('h-nav__item-active');
 				$(this).children('.h-subnav').stop(false, true).fadeOut();
 		});
 	}
-	if ($(window).width() < 991) {
+	if ($(window).width() < 768) {
 		/*header-nav*/
 		$('.h-nav__mob').on('click', function() {
 			$(this).parent().children('.h-nav').slideToggle();
@@ -17,6 +19,7 @@ $(document).ready(function() {
 
 		/*header-subnav*/
 		$('.h-nav__item').on('click', function() {
+			$(this).toggleClass('h-nav__item-active');
 			$(this).children('.h-subnav').stop(false, true).slideToggle();
 			return false;
 		});
